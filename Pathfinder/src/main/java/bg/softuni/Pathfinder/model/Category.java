058705c27141bac0e5f824dee1f03643799ff6db
Rumen Nikolov <45116925+runikolov01@users.noreply.github.com>
2024-06-14 21:30:42 +0300

    Category class is updated

---
+++ Pathfinder/src/main/java/bg/softuni/Pathfinder/model/Category.java
@@ -0,0 +1,52 @@
+package bg.softuni.Pathfinder.model;
+
+import bg.softuni.Pathfinder.model.enums.CategoryType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    private CategoryType name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    public Category() {
+
+    }
+
+    public Category(long id, CategoryType name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public CategoryType getName() {
+        return name;
+    }
+
+    public void setName(CategoryType name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

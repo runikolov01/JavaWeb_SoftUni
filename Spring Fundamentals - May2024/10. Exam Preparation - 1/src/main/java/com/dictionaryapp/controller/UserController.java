ec2ba92e3bba3e07cc9a873feeef984d4ca94480
Rumen Nikolov <45116925+runikolov01@users.noreply.github.com>
2024-06-16 16:23:23 +0300

    RegisterUserDTO is added

--- Spring Fundamentals - May2024/10. Exam Preparation - 1/src/main/java/com/dictionaryapp/controller/UserController.java
+++ Spring Fundamentals - May2024/10. Exam Preparation - 1/src/main/java/com/dictionaryapp/controller/UserController.java
@@ -1,10 +1,23 @@
 package com.dictionaryapp.controller;
 
+import com.dictionaryapp.model.entity.UserRegisterDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
+    @ModelAttribute
+    public UserRegisterDTO createEmptyDTO() {
+        return new UserRegisterDTO();
+    }
+
     @GetMapping("/index")
     public String openIndexPage() {
         return "index";
@@ -15,16 +28,27 @@ public class UserController {
         return "home";
     }
 
+    @GetMapping("/register")
+    public String openRegisterPage(Model model) {
+        return "register";
+    }
+
+    @PostMapping("register")
+    public String registerUser(@Valid UserRegisterDTO data,
+                               BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("registerData", data);
+            return "redirect:/register";
+        }
+        return "redirect:/login";
+    }
 
     @GetMapping("/login")
     public String openLoginPage() {
         return "login";
     }
 
-    @GetMapping("/register")
-    public String openRegisterPage() {
-        return "register";
-    }
 
     @GetMapping("/word-add")
     public String openAddingWordPage() {
